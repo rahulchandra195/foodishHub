be0b1e5b53433a8cad794d87dfef4dd78be867f0
Rahul kumar <42943345+rahulchandra195@users.noreply.github.com>
2023-11-14 13:08:08 +0530

    Add files via upload

---
+++ Home.js
@@ -0,0 +1,15 @@
+import HeroSection from "../components/HeroSection";
+import ImproveSkills from "../components/ImproveSkills";
+import QouteSection from "../components/QuoteSection";
+import ChiefsSection from "../components/ChiefsSection";
+
+export default function Home() {
+  return (
+    <div>
+      <HeroSection />
+      <ImproveSkills />
+      <QouteSection />
+      <ChiefsSection />
+    </div>
+  );
+}
